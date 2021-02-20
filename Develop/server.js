92dd6f48928f040517eafc1906ffddd6674079c8
Nate Schroeder <n8dogg59@hotmail.com>
2021-02-20 11:50:50 -0600

    spellcheck

--- Develop/server.js
+++ Develop/server.js
@@ -5,11 +5,74 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const db = require('./db.json')
+const db = require('./db/db.json')
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+function createNewNote(body, db) {
+  const note = body;
+  db.push(note);
+  fs.writeFile('./db/db.json', JSON.stringify(db), function(err) {
+    if (err) {
+      return console.log(err);
+    } else {
+    console.log("New note added!");
+    }
+  })
+}
+
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  // res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(db);
+})
+
+app.post('/api/notes', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(db);
+    // if (parseInt(db[db.length-1].id) > 900) {
+    //   for(i = 0; i < parseInt(db[db.length-1].id); i++) {
+    //     if (db[i])
+    //   }
+    // }
+    if (db.length === 0) {
+      req.body.id = "0";
+    } else {
+      req.body.id = (parseInt(db[db.length-1].id)+1).toString();
+    }
+      console.log(req.body);
+    // db.push(req.body);
+    // console.log(db);
+    const note = createNewNote(req.body, db);
+    res.json(note);
+  });
+
+app.delete('/api/notes/:id', (req, res) => {
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].id === req.params.id) {
+          db.splice(i, 1);
+          break;
+        }
+    }
+    fs.writeFile('./db/db.json', JSON.stringify(db), function(err) {
+      if (err) {
+        return console.log(err);
+      } else {
+        console.log("Your note was deleted!");
+      }
+      res.json(db);
+    })
+
+})  
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Note Taker Starter Code
+# Note Taker
+
+## Description
+This project helps the user write, save, and delete notes to keep track of tasks that need completed.
+
+## Demonstration Video
+To view a demonstration of how this application works please click on this YouTube link https://www.youtube.com/watch?v=QUHm4Y2-NPg&feature=youtu.be.
+
+## Features
+* Created server.js that connects the front-end and the back-end.
+* The app uses get routes to get the information from the server and send to the user/front-end and post routes to get the information from the user/front-end and add to the server.
+* The app also allows the user to delete notes through a delete() method that deletes the note off the servers json file and also deletes it off the users front-end screen.
+* Used Heroku to make the application production ready.
+
+## Built With
+* JavaScript
+* Node.JS
+* Express.js
+* CSS, HTML, index.js (already provided.)
+
+## Contributing
+Nate Schroeder
+
+## Github Repository
